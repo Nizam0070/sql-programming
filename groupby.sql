ec2f922195ed313f50e67ca5aa39e8f4e5a6f318
Nizam0070 <clouddatabase007@gmail.com>
2026-06-24 16:22:03 +0530

    Update groupby.sql

--- groupby.sql
+++ groupby.sql
@@ -1,4 +1,4 @@
-#group by funtion
+#group by funtion 1
 create database school;
 use school;
 
@@ -16,6 +16,36 @@ iNSERT INTO student VALUES ( 4,"imran","hyderabad",21,90);
 select id from student;
 select city,count(name) from student group by city;
 select city,name,count(id)from student group by city,name;
+
+
+
+
+#group by funtion 2
+create database school;
+use school;
+
+CREATE TABLE payments (
+   customer_id int primary key,
+   customer VARCHAR(50),
+   mode varchar(50),
+   city varchar(50)
+);
+insert into payments
+(customer_id,customer,mode,city )
+values
+(1,"nizam","netbanking","usa"),
+(2,"sadikh","debitcard","pak"),
+(3,"xyz","netbanking","uk"),
+(4,"hidaytath","debitcard","india"),
+(5,"imran","creditcard","israil"),
+(6,"qbc","netbanking","usa"),
+(7,"jkm","debitcard","usa");
+select * from payments;
+select mode,count(customer) from payments group by mode;
+select customer,mode from payments group by customer,mode;
+select * from payments where mode="netbanking" and "debitcard";
+select city,mode from payments group by city,mode;
+select distinct city from payments;
 
 
 
